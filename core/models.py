ea1fb2a50363c6142866970094acd68707547ce1
McleanMartin <chikadayamartin@gmail.com>
2024-07-13 12:06:16 +0200

    custom user base

--- core/models.py
+++ core/models.py
@@ -1,17 +1,47 @@
-import uuid
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
-from .managers import CustomUserManager
+class Tenant(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+    subdomain = models.CharField(max_length=50, unique=True)
 
+    def __str__(self):
+        return self.name
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
+    USER_TYPE_CHOICES = (
+        ('client', 'Client'),
+        ('developer', 'Developer'),
+        ('driver', 'Driver'),
+        ('manager', 'Manager'),
+        ('administration', 'Administration'),
+    )
+
     email = models.EmailField(_("email address"), unique=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
+    user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES, default='client')
+    tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE, related_name='users')
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
@@ -21,13 +51,6 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.email
 
-class Tenant(models.Model):
-    name = models.CharField(max_length=100)
-    address = models.TextField()
-    subdomain = models.CharField(max_length=50, unique=True)
-
-    def __str__(self):
-        return self.name
 
 class Product(models.Model):
     tenant = models.ForeignKey(Tenant, related_name='products', on_delete=models.CASCADE)
